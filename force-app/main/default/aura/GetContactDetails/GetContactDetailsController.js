d172692d212e002e12767e0fd7f1a2ad32ad6ee4
Dinesh Baddawar <69590209+dineshbaddawar@users.noreply.github.com>
2022-09-16 20:21:26 +0530

    Revert "commit 16"

--- force-app/main/default/aura/GetContactDetails/GetContactDetailsController.js
+++ force-app/main/default/aura/GetContactDetails/GetContactDetailsController.js
@@ -11,5 +11,5 @@
             
         });
         $A.enqueueAction(action);
-	},
+	}
 })
--- force-app/main/default/aura/MapValuesInAuraChild/MapValuesInAuraChildController.js
+++ force-app/main/default/aura/MapValuesInAuraChild/MapValuesInAuraChildController.js
@@ -1,6 +1,5 @@
 ({
 	doInit : function(component, event, helper) {
-        debugger;
 		var key = component.get("v.key");
         var map = component.get("v.map");
         component.set("v.value", map[key]);
